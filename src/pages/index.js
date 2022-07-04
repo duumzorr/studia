59a4ad886430703a278bba41afab4c3677704781
Miky-X <kaczmarekmichal94@gmail.com>
2022-07-04 22:15:07 +0200

    commit

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
+        <ContactSection sectionId="github" heading="Issues?" />
+      </Page>
+    </>
+  );
+}
